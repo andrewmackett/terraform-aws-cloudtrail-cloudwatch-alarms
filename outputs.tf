7a88cda734ceaf0f2451c3caa3dab1fa320ef5c7
Andrew Mackett <andrewm@yoyowallet.com>
2019-08-16 17:34:08 +0100

    add sns_topic_name output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "sns_topic_arn" {
   value       = local.sns_topic_arn
 }
 
+output "sns_topic_name" {
+  description = "The ARN of the SNS topic used"
+  value       = aws_sns_topic.default.display_name
+}
+
 output "dashboard_combined" {
   description = "URL to CloudWatch Combined Metric Dashboard"
   value       = "https://console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards:name=${join("", aws_cloudwatch_dashboard.main.*.dashboard_name)}"
